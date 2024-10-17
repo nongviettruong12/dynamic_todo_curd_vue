6ddfed2d4ff21b92f6cc4d12c4a5f4fc6e7f3b5d
nongviettruong12 <nongviettruong12@gmail.com>
2024-10-17 13:37:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+        <div>add todos</div>
+        <input type="text" v-model="value">
+        <button @click="addTodos">Add todos</button>
+       <div>
+            <div>My todoList</div>
+            <ul>
+              <li v-for="item, itemIndex in todoList":key="itemIndex">
+                {{ item }}
+              </li>
+            </ul>
+       </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      todoList: ['go to dentist', 'kill this dog'],
+      value: undefined,
+    };
+  },
+  methods: {
+    addTodos() {
+      this.todoList.push(this.value);
+      this.value = undefined;
+    },
+  }
+  
+};
+</script>
